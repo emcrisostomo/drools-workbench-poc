4cb7c3358d78022b39e0b0440a37d2efacc270a8
Crisostomo, Enrico M <enrico.m.crisostomo@accenture.com>
2021-05-13 15:52:23 +0200

    Add initial skeleton

---
+++ drools-client-poc/src/main/java/com/example/droolsclientpoc/DroolsClientPocApplication.java
@@ -0,0 +1,13 @@
+package com.example.droolsclientpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroolsClientPocApplication
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(DroolsClientPocApplication.class, args);
+    }
+}
---
+++ drools-client-poc/src/test/java/com/example/droolsclientpoc/DroolsClientPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.droolsclientpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroolsClientPocApplicationTests
+{
+    @Test
+    void contextLoads()
+    {
+    }
+}
